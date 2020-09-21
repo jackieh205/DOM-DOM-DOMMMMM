3b83f41ce6995331c1549017539a51b097e1de1b
jackieh205 <jacquelineharper90@gmail.com>
2020-09-21 14:05:11 -0500

    second commit

--- scripts.js
+++ scripts.js
@@ -1,23 +1,32 @@
 window.addEventListener("DOMContentLoaded", function () {
-    let btn = document.createElement("button");
-    btn.innerHTML = "Click Button";
-    document.body.appendChild(btn);
+  let btn = document.createElement("button");
+  btn.innerHTML = "Add Square";
+  document.body.appendChild(btn);
 
-    let container = document.createElement("div");
-    container.classList.add("container");
-    document.body.appendChild(container);
+let squareCounter=1  
 
-    btn.addEventListener("click", function (){
-      let div = document.createElement ("div")
+  let container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+
+  btn.addEventListener("click", function () {
+    let div = document.createElement("div")
     div.classList.add("square")
+    div.style.display = "inline-block"
+    
+  div.id= squareCounter
+  squareCounter ++
 
+  div.innerHTML = div.id
+    
     container.appendChild(div)
 
-
+    
+    
 
   
     })
 
     
-    
+
 })
